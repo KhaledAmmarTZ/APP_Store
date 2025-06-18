ab3c9325f447834cb177b52f2fa789d871e9f81e
Khaled Ammar <khaledammartz@gmail.com>
2025-06-18 12:37:43 +0600

    download tabel added

---
+++ database/migrations/2025_06_18_063644_create_downloads_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('downloads', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id', 20);
+            $table->string('product_id', 15);
+            $table->timestamp('downloaded_at')->useCurrent();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('downloads');
+    }
+};
